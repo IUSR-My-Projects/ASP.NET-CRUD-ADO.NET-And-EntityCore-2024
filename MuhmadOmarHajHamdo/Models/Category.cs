16529d9e6fa2bed19941d39b085f5bd8459ae7f9
MUHMAD OMAR <muhammadaaa727@gmail.com>
2024-05-17 14:24:24 +0300

    add

---
+++ MuhmadOmarHajHamdo/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MuhmadOmarHajHamdo.Models;
+
+public class Category(int id, string name)
+{
+    [Key] public int Id { get; set; } = id;
+
+    [Required, MaxLength(100), MinLength(5)]
+    public string Name { get; set; } = name;
+
+    public static List<Category> Categories { get; set; } = new List<Category>
+    {
+        new Category(1, "Muhmad Category"),
+        new Category(2, "Omar Category"),
+        new Category(3, "Ali Category"),
+    };
+}
