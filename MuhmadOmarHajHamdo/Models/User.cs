5ed97bdd7e6284aa32fdc4a122905dd211105e4c
MUHMAD OMAR <muhammadaaa727@gmail.com>
2024-05-17 14:21:52 +0300

    Add all

---
+++ MuhmadOmarHajHamdo/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace MuhmadOmarHajHamdo.Models;
+
+public class User(int id, string name, int age)
+{
+    public int Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+}
