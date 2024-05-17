213947cc1aac89172988040c2ea85b6c64ff698e
MUHMAD OMAR <muhammadaaa727@gmail.com>
2024-05-18 00:38:26 +0300

    add

---
+++ MuhmadOmarHajHamdo/Entities/20240517205505_first.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MuhmadOmarHajHamdo.Entities
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
--- MuhmadOmarHajHamdo/obj/Debug/net8.0/MuhmadOmarHajHamdo.AssemblyInfo.cs
+++ MuhmadOmarHajHamdo/obj/Debug/net8.0/MuhmadOmarHajHamdo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuhmadOmarHajHamdo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a3c1542585a665ba919604b087bf026fda6542")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4577a37255e8ae3962f2e079086d59364d39e5ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuhmadOmarHajHamdo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuhmadOmarHajHamdo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
