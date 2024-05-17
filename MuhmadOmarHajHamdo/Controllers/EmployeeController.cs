1431a7dd0017cbdf1b001003ec200fd9432a8ba7
MUHMAD OMAR <muhammadaaa727@gmail.com>
2024-05-17 18:31:13 +0300

    a

---
+++ MuhmadOmarHajHamdo/Controllers/EmployeeController.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Mvc;
+using MuhmadOmarHajHamdo.Models;
+using MuhmadOmarHajHamdo.Models.Repositories;
+
+namespace MuhmadOmarHajHamdo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeRepository _employeeRepo = new EmployeeRepository();
+
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            // Get all employees from a database
+            List<Employee> employees = _employeeRepo.GetAllEmployees();
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee? employee = _employeeRepo.GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
